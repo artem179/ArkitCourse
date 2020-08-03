325191d5a1fbd8da987897b5b802d1c1a63dc1db
Artem Shafarostov <artyom179@mail.ru>
2020-08-03 18:33:36 +0300

    Fast implementation of solar system simulation in ARkit + Scenekit.

--- ImageDetection/ImageDetection/ViewController.swift
+++ ImageDetection/ImageDetection/ViewController.swift
@@ -50,10 +50,21 @@ class ViewController: UIViewController {
         sceneView.scene = scene
         self.resetTrackingConfig()
         
+        //Create Earth orbit
+        let earthOrbit = SCNNode()
+        earthOrbit.position = SCNVector3(0, -0.0, 0.0)
+        planetariumNode.addChildNode(earthOrbit)
+        
+        //Create lightning for Sun
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.light?.intensity = 3000
+        
         // Sun
-        let sunNode = SCNNode(geometry: SCNSphere(radius: 0.2))
+        let sunNode = SCNNode(geometry: SCNSphere(radius: 0.08))
         // sunNode.geometry = SCNSphere()
-        sunNode.position = SCNVector3(0, 0, -0.5)
+        sunNode.position = SCNVector3(0, -0.0, 0.0)
         
         let material = SCNMaterial()
         material.diffuse.contents = UIImage(named: "art.scnassets/sunAlbedo.jpg")
@@ -62,20 +73,49 @@ class ViewController: UIViewController {
         
         sunNode.geometry?.firstMaterial = material
         planetariumNode.addChildNode(sunNode)
+        sunNode.addChildNode(lightNode)
         
         //Earth
-        let earthNode = SCNNode(geometry: SCNSphere(radius: 0.1))
-        earthNode.position = SCNVector3(-0.3, 0.0, 0.0)
+        let earthNode = SCNNode(geometry: SCNSphere(radius: 0.03))
+        earthNode.position = SCNVector3(-0.25, -0.0, -0.0)
         earthNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/earthAlbedo.jpg")
-        earthNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/earthEmission.jpg")
+        earthNode.geometry?.firstMaterial?.emission.contents = UIImage(named: "art.scnassets/earthEmission.jpg")
+        earthNode.geometry?.firstMaterial?.emission.intensity = 1.0
         
-        earthNode.geometry?.firstMaterial = material
-        planetariumNode.addChildNode(sunNode)
+        //earthNode.geometry?.firstMaterial = material
+        //planetariumNode.addChildNode(earthNode)
+        
+        //Create Moon orbit
+        let moonOrbit = SCNNode()
+        moonOrbit.position = SCNVector3(-0.25, -0.0, 0.0)
+        earthOrbit.addChildNode(moonOrbit)
+        
+        earthOrbit.addChildNode(earthNode)
         
         //Moon
+        let moonNode = SCNNode(geometry: SCNSphere(radius: 0.01))
+        //moonNode.position = SCNVector3(-1.0, -2.0, -0.5)
+        moonNode.position = SCNVector3(-0.05, 0.0, 0.0)
+        moonNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/moonAlbedo.jpg")
+        //moonNode.geometry?.firstMaterial = material
+        //planetariumNode.addChildNode(moonNode)
+        moonOrbit.addChildNode(moonNode)
         
-        SCNAction.repeatForever(SCNAction())
+        //Planet actions
+        let actionMoon = SCNAction.repeatForever(SCNAction.rotate(by: 2 * .pi, around: SCNVector3(0, 0, -1), duration: 5))
+        let actionSun = SCNAction.repeatForever(SCNAction.rotate(by: .pi/4, around: SCNVector3(0, 0, -1), duration: 5))
+        let actionEarth = SCNAction.repeatForever(SCNAction.rotate(by: .pi/2, around: SCNVector3(0, 0, -1), duration: 5))
         
+        let actionMoonOrbit = SCNAction.repeatForever(SCNAction.rotate(by: 24 * .pi, around: SCNVector3(0, 0, -1), duration: 5))
+        let actionEarthOrbit = SCNAction.repeatForever(SCNAction.rotate(by: .pi, around: SCNVector3(0, 0, -1), duration: 5))
+        
+        
+        sunNode.runAction(actionSun)
+        earthNode.runAction(actionEarth)
+        moonNode.runAction(actionMoon)
+        
+        earthOrbit.runAction(actionEarthOrbit)
+        moonOrbit.runAction(actionMoonOrbit)
     }
     
     func resetTrackingConfig() {
@@ -83,11 +123,11 @@ class ViewController: UIViewController {
             ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
             else { return }
         
-        let config = ARImageTrackingConfiguration()
+        //let config = ARImageTrackingConfiguration()
+        let config = ARWorldTrackingConfiguration()
         
-        config.videoFormat.framesPerSecond
-        config.trackingImages = refImage
-        //config.detectionImages = refImage
+        //config.trackingImages = refImage
+        config.detectionImages = refImage
         config.maximumNumberOfTrackedImages = 1
         
         let options = ARSession.RunOptions([ARSession.RunOptions.removeExistingAnchors,
